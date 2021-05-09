1d3793e28c6cbda21df5996845aa648b1c2c7f73
Sergey Sofeychuk <lolwat97@gmail.com>
2021-05-09 20:44:10 +0100

    suqa

--- print_text.py
+++ print_text.py
@@ -3,6 +3,9 @@
 import sys
 import os
 
+import ntplib
+from time import ctime
+
 import logging
 from waveshare_epd import epd2in13_V2
 import time
@@ -11,18 +14,26 @@ import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
-def draw_screen(epd, font):
+def draw_screen(epd, font, data):
     image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
     draw = ImageDraw.Draw(image)
     
-    draw.text((120, 60), 'penis music', font = font, fill = 0)
+    time = data[0]
+
+    draw.text((0, 0), time, font = font, fill = 0)
     epd.display(epd.getbuffer(image))
 
 def get_info():
-    pass
+    time = get_time()
+    return [time] 
 
+def get_time():
+    response = ntplib.NTPClient().request('europe.pool.ntp.org', version=3)
+    return ctime(response.recv_time)
 
 try:
+    font8 = ImageFont.truetype('Font.ttc', 8)
+    font10 = ImageFont.truetype('Font.ttc', 10)
     font15 = ImageFont.truetype('Font.ttc', 15)
     font24 = ImageFont.truetype('Font.ttc', 24)
 
@@ -32,8 +43,9 @@ try:
     epd.Clear(0xFF)
 
     while(True):
-        get_info()
-        draw_screen(epd, font15)
+        info = get_info()
+        draw_screen(epd, font10, info)
+        time.sleep(60)
     
 except IOError as e:
     logging.info(e)
