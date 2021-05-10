6c91bd940dead890681036f9dbfe0f166d6ad746
lw97 <lolwat97@gmail.com>
2021-05-10 04:04:40 +0300

    forgot the resolution

--- get_twitter_followers.py
+++ get_twitter_followers.py
@@ -6,6 +6,7 @@ PHANTOMJS_PATH = '/home/pi/bin/phantomjs'
 
 driver = webdriver.PhantomJS(PHANTOMJS_PATH)
 #driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS)
+driver.set_window_size(1920, 1080)
 driver.get("https://www.twitter.com/lolwatve")
 time.sleep(5)
 followers_element = driver.find_elements_by_css_selector('.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0.r-b88u0q')[1]
