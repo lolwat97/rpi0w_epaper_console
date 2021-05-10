340754552b28d718c1f04ed3eecbdf307af7b5eb
lw97 <lolwat97@gmail.com>
2021-05-10 10:58:53 +0300

    working twitter scraping, I think

--- get_twitter_followers.py
+++ get_twitter_followers.py
@@ -4,12 +4,21 @@ import time
 
 PHANTOMJS_PATH = '/home/pi/bin/phantomjs'
 
-driver = webdriver.PhantomJS(PHANTOMJS_PATH)
-#driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS)
-driver.set_window_size(1920, 1080)
-driver.get("https://www.twitter.com/lolwatve")
-time.sleep(5)
-followers_element = driver.find_elements_by_css_selector('.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0.r-b88u0q')[1]
-followers = followers_element.text
-print(followers)
-driver.close()
+def open_driver():
+    driver = webdriver.PhantomJS(PHANTOMJS_PATH)
+    #driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS)
+    driver.set_window_size(1920, 1080)
+    return driver
+
+def get_follower_count(driver):
+    try:
+        driver.get("https://www.twitter.com/lolwatve")
+        time.sleep(60)
+        followers_element = driver.find_elements_by_css_selector('.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0.r-b88u0q')[1]
+        followers = followers_element.text
+        return followers
+    except:
+        return 'could not get followers'
+
+def close_driver(driver):
+    driver.close()
--- print_text.py
+++ print_text.py
@@ -4,6 +4,8 @@ import sys
 import os
 import requests
 
+import get_twitter_followers
+
 import ntplib
 from time import ctime
 
@@ -22,6 +24,7 @@ class Console():
         self.font15 = ImageFont.truetype('Font.ttc', 15)
         self.font24 = ImageFont.truetype('Font.ttc', 24)
         self.epd = epd2in13_V2.EPD()
+        self.webdriver = get_twitter_followers.open_driver()
         self.data = []
 
     def init_screen(self):
@@ -35,14 +38,16 @@ class Console():
         draw = ImageDraw.Draw(image)
         
         time = self.data[0]
+        twitter_followers = 'Follower count for @lolwatve: ' + self.data[1]
 
         draw.text((0, 0), time, font = self.font10, fill = 0)
+        draw.text((0, 15), twitter_followers, font = self.font10, fill=0)
         self.epd.display(self.epd.getbuffer(image))
 
     def get_info(self):
         time = self.get_time()
-        fuck = self.get_request()
-        self.data = [time]
+        twitter_followers = get_twitter_followers.get_follower_count(self.webdriver)
+        self.data = [time, twitter_followers]
 
     def get_time(self):
         response = ntplib.NTPClient().request('europe.pool.ntp.org', version=3)
@@ -69,6 +74,7 @@ class Console():
         except KeyboardInterrupt:    
             logging.info("ctrl + c:")
             epd2in13_V2.epdconfig.module_exit()
+            get_twitter_followers.close_driver(self.webdriver)
             exit()
 
 console = Console()
